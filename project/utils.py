e786716856bec8e0118fffea4ff2ac6d915f6f08
PouhoSyun <moodylitchee@outlook.com>
2024-03-29 12:59:26 +0800

    0.4.3

--- project/utils.py
+++ project/utils.py
@@ -8,109 +8,118 @@ import torch.utils.data as data
 import matplotlib.pyplot as plt
 import albumentations as alb
 
-def hvstack16(tensor: torch.Tensor):
-    size = int(tensor.shape[1])
+# 4 basic methods for numpy.array -- fundamental stack/split & fine stack/split functions
+def hvstack(src: np.ndarray):
+    size = int(src.shape[1])
+    pieces = int(src.shape[0] ** 0.5)
     lines = []
-    for line in tensor.reshape(4, 4, size, size):
+    for line in src.reshape(pieces, pieces, size, size):
         lines.append(np.hstack(line))
-    view = np.vstack(lines).astype(np.uint8)
-    return view
+    dst = np.vstack(lines).astype(np.uint8)
+    return dst
 
-def hvsplit16(array: np.ndarray):
-    lines = np.vsplit(array, 4)
-    view = []
-    stack = []
+def hvsplit(src: np.ndarray, pieces = 4):
+    lines = np.vsplit(src, pieces)
+    dst = []
     for line in lines:
-        line = np.hsplit(line, 4)
-        stack.extend(line)
-    return [cell.tolist() for cell in stack]
+        line = np.hsplit(line, pieces)
+        dst.extend(line)
+    return np.array(dst)
 
-# fine stack -- Tensor[16, size, size] -> array[4*size, 4*size]
-def fine_stack16(tensor: torch.Tensor):
-    size = int(tensor.shape[1])
-    array = []
-    for i in range(16):
-        array.append(tensor[:,])
+# fine stack -- array[n*n, size, size] -> array[n*size, n*size]
+def fine_stack(src: np.ndarray):
+    size = int(src.shape[1])
+    pieces = int(src.shape[0] ** 0.5)
+    src = hvstack(src)
+    dst = []
+    for i in range(size * size):
+        dst.append(src[(i//size)::size, (i%size)::size].reshape(pieces, pieces))
+    return hvstack(np.array(dst))
 
-# fine split -- array[4*size, 4*size] -> Tensor[16, size, size]
-def fine_split16(array: np.ndarray):
-    size = int(array.shape[1] / 4)
-    tensor = []
-    for i in range(16):
-        tensor.append(array[(i//4)::4, (i%4)::4].reshape(size, size))
-    return torch.Tensor(tensor)
+# fine split -- array[n*size, n*size] -> array[n*n, size, size]
+def fine_split(src: np.ndarray, pieces = 4):
+    size = int(src.shape[1] / pieces)
+    dst = []
+    for i in range(pieces * pieces):
+        dst.append(src[(i//pieces)::pieces, (i%pieces)::pieces].reshape(size, size))
+    return np.array(dst)
 
 # size of event camera is 190*180
 # transform matfile to streamfile, then use spatial-temporal voxel grid method to record events.
-def pack_event_stream(dataset, file_cnt, time):
-    ev_stream = []
-    for i in range(1, file_cnt + 1):
-        data = mat73.loadmat("test/dataset/" + dataset + "/events_clip/frame" + str(i) + ".mat")
-        events = np.array(tuple(zip(data['clipev']['ev_x'],
-                               data['clipev']['ev_y'],
-                               data['clipev']['ev_p'],
-                               data['clipev']['ev_t'])))
-        ev_stream.append(events)
+def pack_event_stream(dataset, file_cnt, time, split=True):
+    try:
+        event_countmap = np.load("dataset_prep/" + dataset + "/events_voxel.npy")
+    except:
+        ev_stream = []
+        for i in range(1, file_cnt + 1):
+            data = mat73.loadmat("test/dataset/" + dataset + "/events_clip/frame" + str(i) + ".mat")
+            events = np.array(tuple(zip(data['clipev']['ev_x'],
+                                data['clipev']['ev_y'],
+                                data['clipev']['ev_p'],
+                                data['clipev']['ev_t'])))
+            ev_stream.append(events)
 
-    event_countmap = []
-    for events in ev_stream:
-        # DAVIS infrared senser use linear threshold
-        event_field = np.zeros((200, 200))
-        for event in events:
-            event_field[int(event[1])][int(event[0])] += event[2] / (1 + math.exp(time - event[3])) #consider pre/past event affect
-        for it in np.nditer(event_field):
-            it = 255 / (1 + math.exp(-it))
+        event_countmap = []
+        for events in ev_stream:
+            # DAVIS infrared senser use linear threshold
+            event_field = np.zeros((200, 200))
+            for event in events:
+                #consider pre/past event affect
+                event_field[int(event[1])][int(event[0])] += event[2] / (1 + math.exp(time - event[3])) 
+            event_field = np.clip(event_field, -255, 255)
+            for it in np.nditer(event_field, op_flags=["readwrite"]):
+                it[...] = np.uint8(255 / (1 + math.exp(-it)))
 
-        # split the output into 16 50*50 pieces
-        event_countmap.append(hvsplit16(event_field))
+            # split the output into 16 50*50 pieces
+            if not split:
+                event_countmap.append(event_field)
+            else:
+                event_countmap.append(hvsplit(event_field))
 
-    return ev_stream, torch.tensor(event_countmap)
+        event_countmap = np.array(event_countmap)
+        if not os.path.exists("dataset_prep/" + dataset):
+            os.makedirs("dataset_prep/" + dataset)
+        np.save("dataset_prep/" + dataset + "/events_voxel.npy", event_countmap)
+
+    return torch.tensor(event_countmap)
 
 # size of frame camera is 1520 * 1440, then split to 16 400*400 cell-pics.
-def load_frame_png(file_id, cmap, dataset):
-    frame = cv2.cvtColor(cv2.imread("test/dataset/" + dataset + "/RGB_frame/frame" + str(file_id) + ".png"), cmap)
+def load_frame_png(dataset, file_id, cmap, split=True):
+    frame = cv2.imread("test/dataset/" + dataset + "/RGB_frame/frame" + str(file_id) + ".png", cmap)
     h_pad = int((1600 - frame.shape[1]) / 2)
     v_pad = int((1600 - frame.shape[0]) / 2)
-    frame = torch.tensor(cv2.copyMakeBorder(frame, v_pad, v_pad, h_pad, h_pad, cv2.BORDER_CONSTANT, value=(128, 128, 128)))
-    lines = np.vsplit(frame, 4)
-    frame = []
-    for line in lines:
-        line = np.hsplit(line, 4)
-        frame.extend(line)
-    frame = [cell.tolist() for cell in frame]
-    return torch.tensor(frame)
+    frame = cv2.copyMakeBorder(frame, v_pad, v_pad, h_pad, h_pad, cv2.BORDER_CONSTANT, value=(128, 128, 128))
+    if not split: return torch.Tensor(frame)
+    else:
+        lines = np.vsplit(frame, 4)
+        frame = []
+        for line in lines:
+            line = np.hsplit(line, 4)
+            frame.extend(line)
+        return torch.tensor(frame)
 
 def pack_frame_png(dataset, file_cnt, cmap):
     frames = []
     for iter in range(file_cnt):
-        frame = load_frame_png(iter + 1, cmap, dataset)
+        frame = load_frame_png(dataset, iter + 1, cmap)
         frames.append(frame.tolist())
     return torch.tensor(frames)
-
-def show_cell_pics(frame):
-    cv2.namedWindow("Frame PNG", cv2.WINDOW_AUTOSIZE)
-    lines = []
-    for line in frame.reshape(4, 4, 400, 400):
-        lines.append(np.hstack(line))
-    pics = np.vstack(lines).astype(np.uint8)
-    cv2.imshow("Frame PNG", pics)
-    cv2.waitKey(0)
 
 # concatenate tensors of event and frame
 def concat_tensors(dataset, time, cmap):
     iter_size = len(os.listdir("test/dataset/" + dataset + "/RGB_frame"))
     try:
-        frame = np.load("test/dataset/" + dataset + "/concat_frame.npy")
+        frame = np.load("dataset_prep/" + dataset + "/frame.npy")
     except:
         frame = pack_frame_png(dataset, iter_size, cmap).tolist()
-        np.save("test/dataset/" + dataset + "/concat_frame.npy", frame)
+        np.save("dataset_prep/" + dataset + "/frame.npy", frame)
     
     try:
-        events = np.load("test/dataset/" + dataset + "/concat_events.npy") 
+        events = np.load("dataset_prep/" + dataset + "/events_voxel.npy") 
     except:
         __, events = pack_event_stream(dataset, iter_size, time)
         events = events.tolist()
-        np.save("test/dataset/" + dataset + "/concat_events.npy", events)
+        np.save("dataset_prep/" + dataset + "/events_voxel.npy", events)
         
     data = list(zip(frame[0:-1], events, frame[1:]))
     return data
@@ -157,9 +166,9 @@ class DAVIS_Dataset(data.Dataset):
     def __show__(self, index):
         frame, events, target = self.__getitem__(index)
         cv2.namedWindow("Dataset Preview", cv2.WINDOW_FREERATIO)
-        stack_frame = hvstack16(frame)
-        stack_events = cv2.copyMakeBorder(hvstack16(events), 700, 700, 0, 0, cv2.BORDER_CONSTANT, value=(128, 128, 128))
-        stack_target = hvstack16(target)
+        stack_frame = hvstack(frame)
+        stack_events = cv2.copyMakeBorder(hvstack(events), 700, 700, 0, 0, cv2.BORDER_CONSTANT, value=(128, 128, 128))
+        stack_target = hvstack(target)
         show_view = np.hstack([stack_frame, stack_events, stack_target])
         cv2.imshow("Dataset Preview", show_view)
         cv2.waitKey(1)
--- test/test_file/test_utils.py
+++ test/test_file/test_utils.py
@@ -9,22 +9,30 @@ import numpy as np
 from project.utils import *
         
 def test_fine_split(pic_no):
-    win = cv2.namedWindow("win", cv2.WINDOW_FREERATIO)
+    _ = cv2.namedWindow("win", cv2.WINDOW_FREERATIO)
 
     picset = Frame_Dataset("Flowers", cv2.IMREAD_GRAYSCALE, 500)
     pic = np.array(picset[pic_no])
-    split = fine_split16(pic)
-    stack = hvstack16(split)
+    split = fine_split(pic)
+    stack = hvstack(split)
+    fine = fine_stack(split)
 
-    show = np.hstack((pic, stack)).astype(np.uint8)
+    show = np.hstack((pic, stack, fine)).astype(np.uint8)
     cv2.imshow("win", show)
     cv2.waitKey(0)
 
-def test_event_voxel():
-    dataset = DAVIS_Dataset("Indoor4", 0.02, cv2.IMREAD_GRAYSCALE, 1)
-    while(True):
-        dataset.__show__(int(input("Index to preview: ")))
+def test_event_voxel(pic_no):
+    _ = cv2.namedWindow("win", cv2.WINDOW_AUTOSIZE)
+
+    voxel = np.array(pack_event_stream("Indoor4", 200, 0.02, False)[pic_no])
+    # voxel = cv2.copyMakeBorder(voxel, 700, 700, 700, 700, cv2.BORDER_CONSTANT, value=(128, 128, 128))
+    frame = np.array(load_frame_png("Indoor4", pic_no, cv2.IMREAD_GRAYSCALE, False))
+    frame_dst = np.array(load_frame_png("Indoor4", pic_no + 1, cv2.IMREAD_GRAYSCALE, False))
+    # show = np.hstack([frame, voxel, frame_dst]).astype(np.uint8)
+    show = voxel.astype(np.uint8)
+    cv2.imshow("win", show)
+    cv2.waitKey(0)
 
 if __name__ == "__main__":
     # test_fine_split(1)
-    test_event_voxel()
+    test_event_voxel(1)
